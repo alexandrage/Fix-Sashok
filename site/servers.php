76a446682d9a807d8c9b59a6c59542c85ce8e8de
alexandrage <alexandrage@mail.ru>
2016-11-17 01:44:28 +0300

    up

--- site/servers.php
+++ site/servers.php
@@ -5,7 +5,11 @@
 // 4-> Версия клиента
 echo '
 v152, localhost, 25565, 1.5.2<::>
-voxelaria164, 188.165.137.202, 25675, 1.6.4<::>
+vanilla111, localhost, 25565, 1.11.0<::>
+vanilla110, localhost, 25565, 1.10.2<::>
+vanilla187, localhost, 25565, 1.8.9<::>
+voxelaria164, localhost, 25675, 1.6.4<::>
+voxelaria181, localhost, 25675, 1.8.1<::>
+voxelaria189, localhost, 25675, 1.8.9<::>
 voxelaria1710, localhost, 25565, 1.7.10<::>
-vanilla181, localhost, 25565, 1.8.1<::>
 ';
