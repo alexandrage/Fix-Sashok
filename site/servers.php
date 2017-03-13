444090eccda2b94edd994692029bc15dfacb8da6
alexandrage <alexandrage@mail.ru>
2017-03-13 21:42:52 +0300

    up

--- site/servers.php
+++ site/servers.php
@@ -4,12 +4,11 @@
 // 3-> port 
 // 4-> Версия клиента
 echo '
-v152, localhost, 25565, 1.5.2<::>
-vanilla111, localhost, 25565, 1.11.0<::>
-vanilla110, localhost, 25565, 1.10.2<::>
-vanilla187, localhost, 25565, 1.8.9<::>
-voxelaria164, localhost, 25675, 1.6.4<::>
-voxelaria181, localhost, 25675, 1.8.1<::>
-voxelaria189, localhost, 25675, 1.8.9<::>
-voxelaria1710, localhost, 25565, 1.7.10<::>
+v1.5.2, localhost, 25565, 1.5.2<::>
+vanilla1.11.2, localhost, 25565, 1.11.0<::>
+voxelaria1.6.4, localhost, 25675, 1.6.4<::>
+voxelaria1.7.10, localhost, 25565, 1.7.10<::>
+voxelaria1.8.9, localhost, 25675, 1.8.9<::>
+voxelaria1.10.2, localhost, 25565, 1.10.2<::>
+voxelaria1.11.2, localhost, 25565, 1.11.2<::>
 ';
