843c85f6a38957a98d94d6e3369fdfe917fad37c
alexandrage <alexandrage@mail.ru>
2021-06-23 10:46:50 +0300

    up

--- site/servers.php
+++ site/servers.php
@@ -3,6 +3,8 @@
 // 2-> ip 
 // 3-> port 
 // 4-> Версия клиента
+// 5-> Версия forfe
+// 6-> Версия мапингов
 echo '
 v1.5.2, localhost, 25565, 1.5.2<::>
 vanilla1.12.2, localhost, 25565, 1.12.0<::>
@@ -16,4 +18,5 @@ voxelaria1.12.2, localhost, 25565, 1.12.2<::>
 voxelaria1.13.2, localhost, 25565, 1.13.2, fmlclient, 25.0.13, net.minecraftforge, 20190213.203750<::>
 voxelaria1.14.4, localhost, 25565, 1.14.4, fmlclient, 28.1.61, net.minecraftforge, 20190829.143755<::>
 voxelaria1.16.1, localhost, 25565, 1.16.1, fmlclient, 32.0.106, net.minecraftforge, 20200625.160719<::>
+voxelaria1.16.5, localhost, 25565, 1.16.5, fmlclient, 36.1.32, net.minecraftforge, 20210115.111550<::>
 ';
